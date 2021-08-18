a444c9c3a8faa3c5bb382bc26e1b54b33a629174
infaerina <infaerina@gmail.com>
2021-08-17 22:07:08 -0400

    Renamed Script, Updated API
    Updated to use the new API, rather than the old API.

--- J-Novel-Club-Enhancer.user.js
+++ J-Novel-Club-Enhancer.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1.1.1
 // @description  Added part completion to J-Novel-Club as well as an i-frame extender to tc_width_percent value.
-// @downloadURL  https://github.com/infaerina/j-novel-club-userscript/raw/master/J-Novel-ClubWidthExtender.user.js
+// @downloadURL  https://github.com/infaerina/j-novel-club-userscript/raw/master/J-Novel-Enhancer.user.js
 // @author       Infaerina
 // @match        https://j-novel.club/*
 // @icon         https://www.google.com/s2/favicons?domain=j-novel.club
@@ -44,48 +44,25 @@ var tc_some_color_text = "AliceBlue";
 var tc_current_slug;
 
 //Private variables.
-var tc_user_id;
 var tc_parts_json;
-var tc_current_parts_json;
 var tc_parts;
+var tc_type;
 
 var tc_type;
 
 function getParts(){
-    tc_user_id = document.cookie.split('; ').find(row=>row.startsWith('userId=')).split('=')[1].replace('s%3A','').split('.')[0];
-    var s;
-    if(document.querySelectorAll('.novel').length>0){
+    var link;
+    if (document.querySelectorAll('.novel').length > 0){
         tc_type = 'novel';
-        s = 'https://api.j-novel.club/api/users/' + tc_user_id + '/readParts';
-    } else if (document.querySelectorAll('.manga').length>0){
+        link = document.querySelector('a[href$="part-1"]').href;
+    } else if (document.querySelectorAll('.manga').length > 0){
         tc_type = 'manga';
-        s = 'https://api.j-novel.club/api/users/' + tc_user_id + '/mangaReadParts';
+        link = document.querySelector('a[href$="chapter-1"]').href;
     } else {
         return;
     }
-    return fetch(
-        s,
-        {
-            credentials: 'include',
-            method: 'GET'
-        }).then((response)=> response.json()).catch((err)=>{console.log(err)
-    });
-}
-
-function getCurrentParts(){
-    //Always seems to be series.
-    let slug = document.documentURI.split('series/')[1];
-    if(slug.includes('#'))  slug = slug.substring(0, slug.search('#'));
-    var s;
-    if(document.querySelectorAll('.novel').length>0){
-        tc_type = 'novel';
-        s = 'https://api.j-novel.club/api/series/findOne?filter={"where":{"titleslug":"' + slug + '"},"include":["volumes","parts"]}';
-    } else if (document.querySelectorAll('.manga').length>0){
-        tc_type = 'manga';
-        s = 'https://api.j-novel.club/api/mangaSeries/findOne?filter={"where":{"titleslug":"' + slug + '"},"include":["mangaVolumes","mangaParts"]}';
-    } else {
-        return;
-    }
+    let slug = link.replace('https://j-novel.club/read/','');
+    let s = 'https://labs.j-novel.club/app/v1/parts/'+slug+'/toc?format=json';
     return fetch(
         s,
         {
@@ -99,54 +76,29 @@ function getCurrentParts(){
 function populatePartsArray(){
     //create an array equal to the number of parts in current parts.
     var pTemp = [];
-    //Do this to handle manga/novel types
-    var typeSelectorV;
-    var typeSelectorP;
-    var typeSelectorVID;
-    var typeSelectorPID;
-    if (tc_type == "novel"){
-        typeSelectorV = 'volumes';
-        typeSelectorP = 'parts'
-        typeSelectorVID = "volumeId"
-        typeSelectorPID = 'partId';
-    } else if (tc_type == "manga"){
-        typeSelectorV = 'mangaVolumes';
-        typeSelectorP = 'mangaParts';
-        typeSelectorVID = "mangaVolumeId";
-        typeSelectorPID = 'mangaPartId';
-    } else {
-        return;
-    }
-    for (let i = 0; i < tc_current_parts_json[typeSelectorP].length; i++){
-        let p = tc_current_parts_json[typeSelectorP][i];
-        //Remove all non-alpha numeric characters /[^a-zA-Z0-9 -]/
-        var mainTitle = tc_current_parts_json.title.replace(/[^A-Za-z0-9]/g,' ').replace(/\s+/g, " ").trim();
-        var partTitle = p.title.replace(/[^A-Za-z0-9]/g,' ').replace(/\s+/g, " ").trim();
-        var volumeTitle = tc_current_parts_json[typeSelectorV].find(r=>r.id == p[typeSelectorVID]).title.replace(/[^A-Za-z0-9]/g,' ').replace(/\s+/g, " ").trim();
-        var volume = volumeTitle.replace(mainTitle,'').trim();
-        var part = partTitle.replace(mainTitle,'').replace(volume,'').trim();
-        //Find instead of filter, since each part ID is unique.
-        var row = tc_parts_json.find(r=>r[typeSelectorPID] == p.id);
-        var comp;
-        //If it is a manga it will be page number, not percentage.
-        /*
-            They do not store max pages in manga parts, making it impossible to tell if it's fully read. FFS.
-        */
-        if (row){
-            if(tc_type == 'novel'){
-                comp = Math.round(row.completion * 100);
-            } else if (tc_type == 'manga'){
-                comp = row.completion;
-            }
+    //Remove all non-alpha numeric characters /[^a-zA-Z0-9 -]/
+        //Note: I have to do this because of the manga Reborn to Master the Blade, which has an inconsistent title (♀)
+    var mainTitle = tc_parts_json.seriesTitle.replace(/[^A-Za-z0-9]/g,' ').replace(/\s+/g, " ").trim().toLowerCase();
+    for (let i = 0; i < tc_parts_json.parts.parts.length; i++){
+        let p = tc_parts_json.parts.parts[i];
+        var partTitle = p.title.replace(/[^A-Za-z0-9]/g,' ').replace(/\s+/g, " ").trim().toLowerCase();
+        var splits = partTitle.replace(mainTitle,'').match(/\w+ [0-9]+/g);
+        var volume;
+        var part;
+        if(splits.length == 2){
+            volume = splits[0];
+            part = splits[1];
         } else {
-            comp = 0;
+            //This is basically for Ascendance of a Bookworm (Part 1 Volume 2 Part 5, etc)
+            volume = splits[0] + ' ' + splits[1];
+            part = splits[2];
         }
         let t =
             {
-                volume: volume.toLowerCase(),
-                part: part.toLowerCase(),
-                completion: comp,
-                titleslug: tc_current_slug.toLowerCase()
+                volume: volume,
+                part: part,
+                completion: p.progress,
+                titleslug: tc_current_slug
             }
         pTemp.push(t);
     }
@@ -154,7 +106,7 @@ function populatePartsArray(){
 }
 
 function getAPI(){
-    return Promise.all([getParts(), getCurrentParts()]);
+    return Promise.all([getParts()]);
 }
 function setupParts(){
     let newSlug = document.documentURI.split('series/')[1];
@@ -169,8 +121,7 @@ function setupParts(){
         }
     }
     tc_current_slug = newSlug;
-    getAPI().then(([parts, currentParts]) =>{
-        tc_current_parts_json = currentParts;
+    getAPI().then(([parts]) =>{
         tc_parts_json = parts;
         populatePartsArray();
         setupPercentages();
@@ -211,6 +162,7 @@ function setupPercentages(){
                 continue;
             }
             if(tc_type == 'novel'){
+                comp = (Math.round(comp * 1000))/10;
                 if (comp >= tc_percent_complete) {
                     comp = 100;
                     pe[j].style.background = tc_read_color_background;
@@ -230,7 +182,7 @@ function setupPercentages(){
                     pe[j].style.background = tc_some_color_background;
                     pe[j].style.color = tc_some_color_text;
                     let e = document.createElement('span');
-                    let t = document.createTextNode(' pg ' + comp);
+                    let t = document.createTextNode(' (pg: ' + comp + ')');
                     e.appendChild(t);
                     pe[j].appendChild(e);
                 }
